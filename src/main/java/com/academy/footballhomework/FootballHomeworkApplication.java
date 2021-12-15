74fa6623d6e583d4d8a42d3d8b7c8d167af45d3c
Your Name <you@example.com>
2021-12-15 12:41:46 +0200

    Initial commit

---
+++ src/main/java/com/academy/footballhomework/FootballHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.academy.footballhomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballHomeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/academy/footballhomework/FootballHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academy.footballhomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
